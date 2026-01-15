37e108992c8bcdc2948ba76d4c278ba9f5b59b7d
Jena-Eleena <eleenajena2002@gmail.com>
2026-01-15 21:46:45 +0530

    Custom hooks

---
+++ Eleena_Jena_24/day-08-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url)
+        if (!res.ok) throw new Error("Failed to fetch")
+
+        const result = await res.json()
+        setData(result)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return { data, loading, error }
+}
+
+export default useFetch
