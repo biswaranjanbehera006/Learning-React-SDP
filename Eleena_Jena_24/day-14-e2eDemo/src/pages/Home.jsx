ee967d53219f86eca9e6cf574c29eb123672c37b
Jena-Eleena <eleenajena2002@gmail.com>
2026-02-03 12:31:26 +0530

    day14

---
+++ Eleena_Jena_24/day-14-e2eDemo/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div
+      className="bg-white p-8 shadow rounded text-center"
+      data-testid="home-page"
+    >
+      <h1 className="text-2xl font-bold">Welcome 🎉</h1>
+      <p>Login Successful</p>
+    </div>
+  );
+}
+
+export default Home;
